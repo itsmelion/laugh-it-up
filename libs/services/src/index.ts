8754bd29e16dccae9a496c904f5e594cd24028bc
Christhopher Lion <christhopherleao@icloud.com>
2023-06-12 22:21:11 +0200

    updates + add services lib
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

---
+++ libs/services/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/verifyPhone/verifyPhone.services';
---
+++ libs/services/src/lib/verifyPhone/verifyPhone.spec.ts
@@ -0,0 +1,7 @@
+import { services } from './verifyPhone.services';
+
+describe('services', () => {
+  it('should work', () => {
+    expect(services()).toEqual('services');
+  });
+});
