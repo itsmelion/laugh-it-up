630e5146c415a52b2eb0cc31b16724022cf635bd
Christhopher Lion <christhopherleao@icloud.com>
2023-06-16 15:25:19 +0200

    enhance pin input
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- apps/laugh-it-up/app/[category]/verify/PinVerification.tsx
+++ apps/laugh-it-up/app/[category]/verify/PinVerification.tsx
@@ -2,41 +2,44 @@
 import { ErrorMessage } from '@hookform/error-message';
 import { PinInput, Button } from '@laugh-it-up/components';
 import { useVerifyPin } from '@laugh-it-up/services';
-import { Controller, useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 
 export function PinVerification() {
   const verifyPin = useVerifyPin();
   const form = useForm<{ pin: string }>();
 
   return (
-    <form
-      onSubmit={form.handleSubmit((data) => {
-        return verifyPin.execute({
-          pin: data.pin,
-          user_id: 'UUID-CHRIS-LION',
-          country: 'NL',
-        });
-      })}
-    >
-      <Controller
-        name='pin'
-        control={form.control}
-        rules={{
-          required: 'Fill in the pin you got on your phone',
-          minLength: 4
-        }}
-        render={({ field })=> <PinInput {...field} />}
-      />
-      <ErrorMessage
-        name="pin"
-        errors={form.formState.errors}
-        as={<p className='form-error-message'/>}
-      />
-      <Button
-        title='Confirm'
-        type='submit'
-        disabled={verifyPin.loading || form.formState.isDirty && !form.formState.isValid}
-      />
-    </form>
+    <FormProvider {...form}>
+      <form
+        className='gap-2 mt-1'
+        onSubmit={form.handleSubmit((data) => {
+          return verifyPin.execute({
+            pin: data.pin,
+            user_id: 'UUID-CHRIS-LION',
+            country: 'NL',
+          });
+        })}
+      >
+        <PinInput
+          name='pin'
+          rules={{
+            required: 'Fill in the pin you got on your phone',
+            minLength: 4
+          }}
+        />
+
+        <ErrorMessage
+          name="pin"
+          errors={form.formState.errors}
+          as={<p className='form-error-message'/>}
+        />
+
+        <Button
+          title='Confirm'
+          type='submit'
+          disabled={verifyPin.loading || form.formState.isDirty && !form.formState.isValid}
+        />
+      </form>
+    </FormProvider>
   );
 }
--- libs/components/src/lib/PinInput/PinInput.tsx
+++ libs/components/src/lib/PinInput/PinInput.tsx
@@ -1,10 +1,12 @@
 'use client'
-import React, { forwardRef, useRef, useState } from 'react'
+import React, { forwardRef, useCallback, useRef, useState } from 'react';
+import { useController, UseControllerProps } from 'react-hook-form';
 
-import styles from './PinInput.module.css'
+import styles from './PinInput.module.css';
 
-export const PinInput = forwardRef<HTMLInputElement>((props, ref) => {
-  const [pin, setPin] = useState<string[]>(new Array(4).fill(''));
+export const PinInput = forwardRef<HTMLInputElement, UseControllerProps<{ pin: string }>>((props) => {
+  const { field } = useController(props);
+  const [pin, setPin] = useState<string[]>(field.value?.split('') || new Array(4).fill(''));
   const inputRefs = useRef<HTMLInputElement[]>([]);
 
   const focusPinAt = (pinAt = 0) => {
@@ -12,34 +14,38 @@ export const PinInput = forwardRef<HTMLInputElement>((props, ref) => {
     if (input) input.focus();
   };
 
-  const onChange = (ev: React.ChangeEvent<HTMLInputElement>, idx = 0) => {
+  const onChange = useCallback((ev: React.ChangeEvent<HTMLInputElement>, idx = 0) => {
     const {value} = ev.target;
     if (!value) return;
 
     setPin((old) => {
       const newVal = [...old];
       newVal[idx] = value.slice(-1);
+      field.onChange(newVal.join().replace(/,/g, '')); // send change to form
       return newVal;
     });
 
     // if thers another field, shift focus to next one.
     if (idx < 4) focusPinAt(idx + 1);
-  };
+    else field.onBlur();
+  }, [field]);
 
-  const handleBackspace = (ev: React.KeyboardEvent<HTMLInputElement>, idx = 0) => {
+  const handleBackspace = useCallback((ev: React.KeyboardEvent<HTMLInputElement>, idx = 0) => {
     const { code } = ev.nativeEvent;
     if (code !== 'Backspace') return;
 
     focusPinAt(Math.max(0, idx - 1));
+
     setPin((old) => {
       const newVal = [...old];
       newVal[idx] = '';
+      field.onChange(newVal.join().replace(/,/g, '')); // send change to form
       return newVal;
     });
-  };
+  }, [field]);
 
   return (
-    <fieldset className={styles.wrapper}>
+    <fieldset className={styles.wrapper} ref={field.ref}>
       {pin.map((v, i) => (
         <input
           {...props}
@@ -50,6 +56,7 @@ export const PinInput = forwardRef<HTMLInputElement>((props, ref) => {
           onChange={(ev) => onChange(ev, i)}
           key={i}
           autoFocus={i === 0}
+          autoComplete='one-time-code'
           value={v || ''}
           maxLength={1}
           min={0}
